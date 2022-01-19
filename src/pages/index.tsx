fef92c90c54309d01d693eeb067167d77c563abc
Vinicius Flores <viniciusflores379@gmail.com>
2022-01-19 18:37:52 -0300

    :construction: Structure of app

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home(){
+    return (
+        <>
+            <Head>
+                <title>My Home</title>
+            </Head>
+            <h1>
+                Hello Next World
+            </h1>
+        </>
+    )
+}
