f664d23dacb5c6031d95a603f2cc7bf9445a086d
Vinicius Flores <viniciusflores379@gmail.com>
2022-01-21 16:13:12 -0300

    :construction: Created home style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
 
-export default function Home(){
-    return (
-        <>
-            <Head>
-                <title>My Home</title>
-            </Head>
-            <h1>
-                Hello Next World
-            </h1>
-        </>
-    )
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1> News about the <span> React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  )
 }
